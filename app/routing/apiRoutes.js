2833676eb10cc40a84ff2ffe66e66cd53a6c6522
Julio Bell <juliobell2014@gmail.com>
2019-05-24 20:14:01 -0400

    created and updated all files and folders. Added bootstrap and MDB framework onto the project repo, included extra styling

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,30 +1,71 @@
 //Includes the data on friendsArray stored in friends.js file
-var friendsData = require('../data/friends.js');
+var friends = require('../data/friends.js');
 
-//Exporting the data to another js files
+// //Exporting the data to another js files
 module.exports = function(app) {
-    //Display the tableData in json format
     app.get('/api/friends', function(req, res){
-        res.json(friendsData);
+		res.json(friends);
+	});
+
+
+// Create new user - takes in JSON input
+app.post("/api/friends", function(req, res) {
+	 //Comparing user with their best friend match 
+    //Object to hold the best match
+	var totalDifference = 0;
+	var bestMatch = {
+		name: "",
+		photo: "",
+		difference: 1000 //Make the initial value of difference BIG for comparison
+    };
+    
+    // Here we take the result of the user's survey POST and parse it.
+    var userInput = req.body;
+    var userName = userInput.name;
+    var userScores = userInput.scores;
+    // Converting the users score to a number (Instead of string)
+    var b = userScores.map(function(item) {
+        return parseInt(item, 10);
     });
 
-    // //Update with POST the friendsData when a new reservation is made
-    //  app.post('/api/friends', function(req, res){
-    //     if(friendsData.length < 5) {
-    //         friendsData.push(req.body);
-    //         res.json(true);
-    //     } else {
-    //         waitListData.push(req.body);
-    //         res.json(false);
-    //     }
-    // });
+    userInput = {
+        name: req.body.name,
+        photo: req.body.photo,
+        scores: b
+    };
+    
+    console.log("Name: " + userName);
+    console.log("User Score: " + userScores);
+    // Converting the users score to a sum number (Adds up all the numbers in array)
+    var sum = b.reduce((a, b) => a + b, 0);
+    console.log("Sum of user's score: " + sum);
+    console.log("Best match friend difference: " + bestMatch.difference);
 
-    // //Clear with POST the tableData and waitingListData when you click on 'Clear Table' link
-    // app.post('/api/clear', function(){
-    //     tableData = [];
-    //     waitListData = [];
-    //     console.log(tableData);
-    //     console.log(waitListData);
-    // });
-
+	// Loop through all the friend possibilities in the database. 
+	for (var i = 0; i < friends.length; i++) {
+		totalDifference = 0;
+        console.log(friends[i].name);
+        
+        console.log("Best match friend difference: " + bestMatch.difference);
+        for (var j = 0; j < userInput.scores.length; j++) {
+		// for (var j = 0; j < friends[i].scores.length; j++) {
+			totalDifference += Math.abs(friends[i].scores[j] - userInput.scores[j]);
+            
+            // If the sum of differences is less then the differences of the current "best match"
+			if (totalDifference <= bestMatch.difference) {
+				bestMatch.name = friends[i].name,
+				bestMatch.photo = friends[i].photo,
+				bestMatch.difference = totalDifference
+            }  
+        }
+        console.log("Total Difference: " + totalDifference);
+    }
+    console.log(bestMatch);
+    friends.push(userInput);
+    console.log("New User added");
+    console.log(userInput);
+    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page.
+    res.json(bestMatch);
+    
+});
 }
--- server.js
+++ server.js
@@ -1,20 +1,27 @@
+// Series of npm packages
 var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path'); //Node.js built-in package
 var app = express();
-var PORT = process.env.PORT || 8080;
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
- 
-// parse application/json
+
+// Sets an initial port.
+var PORT = process.env.PORT || 3000; 
+
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static(__dirname + "/app/public"));
 
-//All the Routes go here..
+
+//The Routes...
 //Includes html-routes.js and api-routes.js in the server.js file and use Express
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
+// Start the server to begin listening
 var server = app.listen(PORT, function () {
     console.log('App listening on PORT: ' + PORT);
 });
